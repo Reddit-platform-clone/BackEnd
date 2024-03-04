a4cdadf4fd3a9d11cc009ef192ac6e0f2d5f238c
Zyad Mohamed Sowilam <96208685+Zyadsowilam@users.noreply.github.com>
2024-03-05 00:15:48 +0200

    message

--- routes/messageRoutes.js
+++ routes/messageRoutes.js
@@ -1,11 +1,29 @@
-// messageRoutes.js
-
 /**
  * @swagger
  * /api/message/compose:
  *   post:
  *     summary: Send a private message
  *     description: Send a private message to a recipient
+ *     parameters:
+ *       - in: body
+ *         name: message
+ *         description: Message details
+ *         required: true
+ *         schema:
+ *           type: object
+ *           properties:
+ *             recipient:
+ *               type: string
+ *               description: Username of the recipient.
+ *             from:
+ *               type: string
+ *               description: Username of the current user sending the message.
+ *             title:
+ *               type: string
+ *               description: Title of the message.
+ *             content:
+ *               type: string
+ *               description: Content of the message.
  *     requestBody:
  *       required: true
  *       content:
@@ -16,6 +34,12 @@
  *               recipient:
  *                 type: string
  *                 description: Username of the recipient
+ *               from:
+ *                 type: string
+ *                 description: Username of the current user sending the message.
+ *               title:
+ *                 type: string
+ *                 description: Title of the message.
  *               message:
  *                 type: string
  *                 description: Content of the message
@@ -29,16 +53,18 @@
  *               properties:
  *                 success:
  *                   type: boolean
+ *                   description: Indicates whether the message was sent successfully.
  *                   example: true
  *                 message:
  *                   type: string
+ *                   description: A success message indicating the status of the operation.
  *                   example: Message sent successfully
  */
 
 
 const express = require('express');
 const router = express.Router();
-const messageController = require('../controllers/messageController.js'); // Import messageController
+const messageController = require('../controllers/messageController.js');
 
 // Define route for sending private message
 router.post('/compose', messageController.compose);
