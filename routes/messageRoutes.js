48680188525de553faf6510450a1a538d72e9b59
Zyad Mohamed Sowilam <96208685+Zyadsowilam@users.noreply.github.com>
2024-03-04 23:12:13 +0200

    swag

--- routes/messageRoutes.js
+++ routes/messageRoutes.js
@@ -1,3 +1,39 @@
+// messageRoutes.js
+
+/**
+ * @swagger
+ * /api/message/compose:
+ *   post:
+ *     summary: Send a private message
+ *     description: Send a private message to a recipient
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               recipient:
+ *                 type: string
+ *                 description: Username of the recipient
+ *               message:
+ *                 type: string
+ *                 description: Content of the message
+ *     responses:
+ *       '200':
+ *         description: Message sent successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                   example: true
+ *                 message:
+ *                   type: string
+ *                   example: Message sent successfully
+ */
 
 
 const express = require('express');
--- server.js
+++ server.js
@@ -1,14 +1,18 @@
-// server.js
+
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const messageRoutes = require('./routes/messageRoutes.js'); 
+const { swaggerUi, specs } = require('./swaggerConfig');
+const messageRoutes = require('./routes/messageRoutes.js');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(bodyParser.json());
+
+// Use Swagger middleware
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
 
 // Use messageRoutes
 app.use('/api/message', messageRoutes);
