05ca2e1b49e531dff5a7008c9da25d52607eacaf
Zyad Mohamed Sowilam <96208685+Zyadsowilam@users.noreply.github.com>
2024-03-04 23:07:07 +0200

    swagger

---
+++ swaggerConfig.js
@@ -0,0 +1,29 @@
+
+
+const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
+const options = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'Messaging API',
+            version: '1.0.0',
+            description: 'API documentation for messaging service',
+        },
+        servers: [
+            {
+                url: 'http://localhost:3000', // Change this to match your server URL
+                description: 'Development server',
+            },
+        ],
+    },
+    apis: ['./routes/*.js'], // Path to the API routes files
+};
+
+const specs = swaggerJsdoc(options);
+
+module.exports = {
+    swaggerUi,
+    specs,
+};
