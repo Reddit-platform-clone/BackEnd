fab1b9e2a7bc968b5f005081f169ea72d7d3fecc
Zyad Mohamed Sowilam <96208685+Zyadsowilam@users.noreply.github.com>
2024-03-05 23:24:36 +0200

    serv

---
+++ services/messageService.js
@@ -0,0 +1,30 @@
+
+
+const Message = require('../models/messageModel.js');
+
+const messageService = {
+    composeMessage: async (messageData) => {
+        // Logic to create a new message
+        
+        // return newMessage;
+    },
+    
+    getInboxMessages: async (userId) => {
+        // Logic to retrieve inbox messages for the specified user
+        // Example: const inboxMessages = await Message.find({ recipient: userId });
+        // return inboxMessages;
+    },
+    
+    getUnreadMessages: async (userId) => {
+        // Logic to retrieve unread messages for the specified user
+        // Example: const unreadMessages = await Message.find({ recipient: userId, status: 'unread' });
+        // return unreadMessages;
+    },
+
+    deleteMessage: async (messageId) => {
+       
+        // Example: await Message.findByIdAndDelete(messageId);
+    }
+};
+
+module.exports = messageService;
