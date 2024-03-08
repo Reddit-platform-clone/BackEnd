a7e80aed9a1b166b7d626cbda6548e10ad3b1fc5
Yousef Wael <yousef.wael2002@gmail.com>
2024-03-08 11:05:37 +0200

    documented best subreddit

--- server.js
+++ server.js
@@ -2,7 +2,10 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const messageRoutes = require('./routes/messageRoutes.js'); 
+const messageRoutes = require('./routes/messageRoutes.js');
+const { swaggerUi, specs, router } = require('./swaggerConfig.js')
+const userRoute = require('./routes/userRoute.js') 
+const subredditRoute = require('./routes/subredditRoutes.js');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -10,8 +13,13 @@ const PORT = process.env.PORT || 3000;
 // Middleware
 app.use(bodyParser.json());
 
+// Swagger middleware
+app.use('/api-docs', router);
+
 // Use messageRoutes
 app.use('/', messageRoutes);
+app.use('/', userRoute)
+app.use('/', subredditRoute)
 
 // Start the server
 app.listen(PORT, () => {
