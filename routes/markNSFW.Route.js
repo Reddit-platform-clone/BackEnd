243dcc0a25e5662eafe6230eee34e9e2cab5110e
Zyad Mohamed Sowilam <96208685+Zyadsowilam@users.noreply.github.com>
2024-03-09 03:09:03 +0200

    unmrk

--- routes/markNSFW.Route.js
+++ routes/markNSFW.Route.js
@@ -3,6 +3,6 @@ const express = require('express');
 const router = express.Router();
 const nsfwController = require('../controllers/markNSFWController');
 
-router.post('/api/marknsfwmodposts', nsfwController.markNsfwModPosts);
+router.post('/api/marknsfw', nsfwController.markNsfwModPosts);
 
 module.exports = router;
