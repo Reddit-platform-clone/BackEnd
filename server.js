61a250c3ca16af437d9f5c226e6e0c6e8db7ef7b
Yousef Wael <yousef.wael2002@gmail.com>
2024-03-09 09:59:13 +0200

    adjusted server

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const messageRoutes = require('./routes/messageRoutes.js');
+const messageRoutes = require('./routes/messageRoute.js');
 const { swaggerUi, specs, router } = require('./swaggerConfig.js')
 const userRoute = require('./routes/userRoute.js') 
 const subredditRoute = require('./routes/subredditRoutes.js');
@@ -26,7 +26,7 @@ app.use('/', subredditRoute)
 app.use('/api-docs', router); 
 
 
-app.use('/', messageRoute);
+app.use('/', messageRoutes);
 app.use('/', userRoute);
 // Start the server
 app.listen(PORT, () => {
