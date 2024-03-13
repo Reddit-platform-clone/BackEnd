bf6a18e8d8388dd8cba737173a6baf0f1b75b030
RadwaAbdelfattah <101428006+RadwaAbdelfattah@users.noreply.github.com>
2024-03-14 00:48:35 +0200

    shelt follow post

--- routes/followRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-
-const router = express.Router();
-const followController = require('../controllers/followController');
-
-router.post('/api/follow_post', followController.followPost);
-
-module.exports = router;
