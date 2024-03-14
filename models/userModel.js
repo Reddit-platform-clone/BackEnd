f8b25f61d7d3aeb0010b0a172c9c5281345d4c88
AbdullahAhmed30 <abdullah.attia021@eng-st.cu.edu.eg>
2024-03-15 01:16:39 +0200

    user model

--- models/userModel.js
+++ models/userModel.js
@@ -1,6 +1,74 @@
-// Define the friends schema or model
+const mongoose = require('mongoose');
+require('dotenv').config();
+
 const userSchema = {
-  // Define schema fields here
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+
+  password: {
+    type: String,
+    required: true
+  },
+
+  email: {
+    type: String,
+    required: true
+  },
+
+  dateOfBirth: {
+    type: String,
+    required: true
+  },
+
+  displayName: {
+    type: String,
+    required: true,
+    unique: true
+  },
+
+  profilePicture: {
+    type: String,
+  },
+
+  followers: {
+    type: [String],
+    default: []
+  },
+
+  blockedUsers: {
+    type: [String],
+    default: []
+  },
+
+  about: {
+    type: String
+  },
+
+  interests: {
+    type: [String],
+    default: [],
+    required: true
+  },
+
+  gender: {
+    type: Boolean,
+    required: true
+  },
+
+  socialLinks: {
+    type: [String],
+    default: []
+  },
+
+  token: {
+    type: String,
+    required: true
+  },
 };
 
-module.exports = userSchema;
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
