607a6dd7e3ad4d4943d723746ea862491e846453
AbdullahAhmed30 <abdullah.attia021@eng-st.cu.edu.eg>
2024-03-16 16:50:25 +0200

    switched from local db to shared db

--- TestingSchema/user.test.js
+++ TestingSchema/user.test.js
@@ -5,7 +5,7 @@ const e = require('express');
 const exp = require('constants');
 
 beforeAll(async () => {
-  await mongoose.connect('mongodb://localhost:27017/', {
+  await mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true
