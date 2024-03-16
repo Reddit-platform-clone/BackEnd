4c35129c513f02f0ca29b800147c2c890c7e80e7
AbdullahAhmed30 <abdullah.attia021@eng-st.cu.edu.eg>
2024-03-16 03:58:35 +0200

    successful tests

--- TestingSchema/user.test.js
+++ TestingSchema/user.test.js
@@ -1,10 +1,9 @@
 const mongoose = require('mongoose');
 
-const Comment = require('../models/userModel');
-const { describe, before, it } = require('node:test');
+const User = require('../models/userModel');
 
 beforeAll(async () => {
-  await mongoose.connect('mongodb://localhost:27017/testDB', {
+  await mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true
@@ -12,25 +11,86 @@ beforeAll(async () => {
 });
 
 describe('User Model Test', () => {
-    beforeEach(async () => {
-        await User.deleteMany({});
-    });
+  beforeEach(async () => {
+      await User.deleteMany({});
+  });
 
-    it('should be able to insert a user into the database', async () => {
-        const userData = {
-            username: 'user123',
-            password: 'password123',
-            email: 'user@gmail.com',
-            dateOfBirth: '1999-01-01',
-            displayName: 'myuser',
-            profilePicture: 'profile.jpg',
-            followers: [],
-            blockedUsers: [],
-            about: 'I am a user',
-            interests: ['music', 'sports'],
-            gender: 'Male',
-            socialLinks: [],
-            token: 'token123'
-        }
-    })
-})
+  it('should be able to insert a user into the database', async () => {
+      const userData = {
+          username: 'user123',
+          password: 'password123',
+          email: 'user@gmail.com',
+          dateOfBirth: '1999-01-01',
+          displayName: 'myuser',
+          profilePicture: 'profile.jpg',
+          followers: [],
+          blockedUsers: [],
+          about: 'I am a user',
+          interests: ['music', 'sports'],
+          gender: 'Male',
+          socialLinks: [],
+          token: 'token123'
+      };
+
+      const user = new User(userData);
+      const savedUser = await user.save();
+
+      expect(savedUser._id).toBeDefined();
+      expect(savedUser.username).toBe(userData.username);
+      expect(savedUser.password).toBe(userData.password);
+      expect(savedUser.email).toBe(userData.email);
+      expect(savedUser.dateOfBirth).toBe(userData.dateOfBirth);
+      expect(savedUser.displayName).toBe(userData.displayName);
+      expect(savedUser.profilePicture).toBe(userData.profilePicture);
+      expect(savedUser.followers.toObject()).toEqual(userData.followers);
+      expect(savedUser.blockedUsers.toObject()).toEqual(userData.blockedUsers);
+      expect(savedUser.about).toBe(userData.about);
+      expect(savedUser.interests.toObject()).toEqual(userData.interests);
+      expect(savedUser.gender).toEqual(userData.gender);
+      expect(savedUser.socialLinks.toObject()).toEqual(userData.socialLinks);
+      expect(savedUser.token).toBe(userData.token);
+  });
+
+  it('should be able to retrieve all users from the database', async () => {
+    const user1 = {
+      username: 'user123',
+      password: 'password123',
+      email: 'user@gmail.com',
+      dateOfBirth: '1999-01-01',
+      displayName: 'myuser',
+      profilePicture: 'profile.jpg',
+      followers: [],
+      blockedUsers: [],
+      about: 'I am a user',
+      interests: ['music', 'sports'],
+      gender: 'Male',
+      socialLinks: [],
+      token: 'token123'
+    };
+    const user2 = {
+      username: 'user456',
+      password: 'password456',
+      email: 'user2@gmail.com',
+      dateOfBirth: '2002-01-01',
+      displayName: 'user2',
+      profilePicture: 'profile2.jpg',
+      followers: [],
+      blockedUsers: [],
+      about: 'I am user2',
+      interests: ['frisbee', 'reading'],
+      gender: 'Female',
+      socialLinks: [],
+      token: 'token456'
+    };
+
+    await User.create(user1);
+    await User.create(user2);
+
+    const users = await User.find({});
+    expect(users.length).toBe(2);
+  });
+});
+
+afterAll(async () => {
+  await mongoose.connection.close();
+});
