aa4e5ce1022578d1cfe40490f820fe768fbe2117
Zyad Sowilam <96208685+Zyadsowilam@users.noreply.github.com>
2024-03-26 18:50:44 +0200

    Update deleteMessage.test.js

--- unitTesting/deleteMessage.test.js
+++ unitTesting/deleteMessage.test.js
@@ -1,40 +1,21 @@
-const messageService = require('../services/messageService');
-const messageController = require('../controllers/messageController');
-const { validationResult } = require('express-validator');
-const UserModel = require('../models/userModel');
 
-jest.mock('../services/messageService');
-jest.mock('../models/userModel');
-jest.mock('express-validator');
+const request = require('supertest');
+const app = require('../server'); 
 
-describe('Message Controller', () => {
-    describe('delete', () => {
-        it('should successfully delete  a message', async () => {
-           
-            const req = {
-                headers: {
-                    authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6Inp5YWQiLCJpYXQiOjE3MTE0MTc1NjZ9.3VJdo_nz1cHd7nVAdizTCILET4FTMyjz8b7VpVsXJh0'
-                },
-                body: {
-                    "_id": "66027496b4d8270e5c5a0684"
-                }
-            };
-            const res = {
-                status: jest.fn().mockReturnThis(),
-                json: jest.fn()
-            };
+describe('DELETE /message/del_msg', () => {
+  it('should delete a  message', async () => {
+    const token = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6Inp5YWQiLCJpYXQiOjE3MTE0MTc1NjZ9.3VJdo_nz1cHd7nVAdizTCILET4FTMyjz8b7VpVsXJh0'; // Replace with a valid token for testing
+    const messageData = {
+        _id: "66024a20b98ea4302462f5ad"
+    };
 
-        
-            
-            messageService.deleteMessage.mockResolvedValue({ success: true, message: 'Message deleted successfully.' });
+    const response = await request(app)
+      .delete('/message/del_msg')
+      .set('Authorization', token)
+      .send(messageData);
 
-            
-            await messageController.deleteMessage(req, res);
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toHaveProperty('message', 'Message deleted successfully.');
 
-            expect(res.status).toHaveBeenCalledWith(200);
-            expect(res.json).toHaveBeenCalledWith({ message: 'Message deleted successfully.' });
-        });
-        
-
-        });
+  });
 });
