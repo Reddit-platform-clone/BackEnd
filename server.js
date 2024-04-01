21680e04197b578d846cd7b875bca6d8777a8881
Yousef Wael <yousef.wael2002@gmail.com>
2024-04-01 15:40:07 +0200

    added cors

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const fs = require('fs');
 const path = require('path');
 const { swaggerUi, specs, router } = require('./swaggerConfig');
@@ -16,6 +17,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
+app.use(cors());
 
 app.use('/api-docs', router);
 
