1391940daeca1fe039425c5dda013844cc7f35ad
AbdullahAhmed30 <abdullah.attia021@eng-st.cu.edu.eg>
2024-04-02 14:19:42 +0200

    getFriendInfo tests modified

--- userUnitTesting/getFriendInfo.test.js
+++ userUnitTesting/getFriendInfo.test.js
@@ -2,18 +2,36 @@ const request = require('supertest');
 // const app = require('../server');
 
 describe('GET /api/v1/me/friends/:username', () => {
-    it('should block a user', async () => {
-        token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImtvZnRhIDNhbGZhN20iLCJpYXQiOjE3MTE0OTkxNDZ9.vEvVmG0O5d39fPB9aApiYvyFI5wqrHwtq87v68wxHSs'
+    it('should get info of a friend', async () => {
+        token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImJlYm9AZ21haWwuY29tIiwiaWF0IjoxNzEyMDU5ODY3fQ.NV5opXmReq0vo-LVrvT6OiQsthwrAM6mAJEfajYVPto'
         const response = await request('http://localhost:5000/')
-            .get('api/v1/me/friends/abdallah')
+            .get('api/v1/me/friends/wael')
             .set('Authorization', 'Bearer ' + token);
 
         expect(response.statusCode).toBe(200);
     }),
 
-    it('should not block a user with missing an unauthorized token', async () => {
+    it('should not get a friend info with request missing authorization token', async () => {
         const response = await request('http://localhost:5000/')
             .get('api/v1/me/friends/abdallah');
         expect(response.statusCode).toBe(401);
+    }),
+
+    it('should not get info with request with invalid token', async () => {
+        token = 'yJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImJlYm9AZ21haWwuY29tIiwiaWF0IjoxNzEyMDU5ODY3fQ.NV5opXmReq0vo-LVrvT6OiQsthwrAM6mAJEfajYVPto'
+        const response = await request('http://localhost:5000/')
+            .get('api/v1/me/friends/wael')
+            .set('Authorization', 'Bearer ' + token);
+
+        expect(response.statusCode).toBe(403);  
+    }),
+
+    it('should not get infor of a user who is not a friend', async () => {
+        token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImJlYm9AZ21haWwuY29tIiwiaWF0IjoxNzEyMDU5ODY3fQ.NV5opXmReq0vo-LVrvT6OiQsthwrAM6mAJEfajYVPto'
+        const response = await request('http://localhost:5000/')
+            .get('api/v1/me/friends/abdallah')
+            .set('Authorization', 'Bearer ' + token);
+
+        expect(response.statusCode).toBe(400);
     })
 })
