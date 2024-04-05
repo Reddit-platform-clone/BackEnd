fabab02f3906a39adfc6aee9269d1e29e2cd3179
Yousef Wael <yousef.wael2002@gmail.com>
2024-04-05 17:50:31 +0200

    added search by service

--- services/searchByServices.js
+++ services/searchByServices.js
@@ -1,34 +1,48 @@
 const sort = require('../models/searchByModel.js');
+const User = require('../models/userModel.js');
+const Post = require('../models/postModel.js');
+const Comment = require('../models/commentModel.js');
+const Community = require('../schemas/communitySchema.js');
+const Hashtag = require('../models/hashtagModel.js');
+
 
 const searchByService = {
     searchByUsers: async (userId) => {
         // Logic to search by users
-         
-        // return users results;
+        const usersResults = await User.findById(userId);
+        return usersResults;
     },
     
     searchByPosts: async (post) => {
         // Logic to search by posts
+        let postsResults = await Post.find(post);
         //Logic for Trending today
+        const today = new Date();
+        today.setHours(0, 0, 0, 0);
+        postsResults = postsResults.filter(post => post.createdAt >= today);
         //Logic for Sorting
+        postsResults.sort((a, b) => b.createdAt - a.createdAt);
         // return posts results
+        return postsResults;
     },
     
     searchByComments: async (commentId) => {
         // Logic to search by comments
-        //Logic for sorting
-        // return comments results;
+        const commentsResults = await Comment.findById(commentId).sort({ createdAt: -1 });
+        return commentsResults;
     },
 
     searchByCommunities: async (communityId) => {
         // Logic to search by communities
-    
-        // return communities results;
+        const communitiesResults = await Community.findById(communityId);
+        return communitiesResults;
     },
 
     searchByHashtags: async (hashtags) => {
-        //logic to search by hashtags
-        //return hashtags results
+        // Logic to search by hashtags
+        const hashtagsResults = await Hashtag.find({ name: { $in: hashtags } });
+        return hashtagsResults;
+
     }
 };
 
