9b03faef1723ffd425af3961c918c40e760b7e0d
Zyad Sowilam <96208685+Zyadsowilam@users.noreply.github.com>
2024-04-14 22:21:29 +0200

    hide

--- controllers/hideController.js
+++ controllers/hideController.js
@@ -1,12 +1,58 @@
+
+hideService=require('../services/hideService')
 const hideController = {
 
   hideLink: async (req, res) => {
     try {
-      res.json({ success: true, message: 'Link linkId hidden successfully' });
-    } catch (error) {
-      res.status(500).json({ success: false, message: 'Failed to hide link', error: error.message });
-    }
+      const {entityId,type} = req.body;
+ 
+      let username =req.user;
+      
+      if (req.user?.iat){
+        username=req.user.username;
+      }
+  else{
+    username=req.user;
+  }
+
+    const result=await hideService.hidePostOrComment(entityId, username,type );
+    
+    if (result.success) {
+    
+    res.status(200).json({ message: 'hide  successfully.' });
+  } else {
+    res.status(400).json({ errors: result.errors, message: result.error });
+}
+} catch (error) {
+  res.status(500).json({ success: false, message: 'Failed to hide', error: error.message });
+}
   },
+  unhideLink: async (req, res) => {
+    try {
+      const {entityId,type} = req.body;
+ 
+      let username =req.user;
+      
+      if (req.user?.iat){
+        username=req.user.username;
+      }
+  else{
+    username=req.user;
+  }
+
+    const result=await hideService.unhidePostOrComment(entityId, username,type );
+    
+    if (result.success) {
+    
+    res.status(200).json({ message: 'unhide  successfully.' });
+  } else {
+    res.status(400).json({ errors: result.errors, message: result.error });
+}
+} catch (error) {
+  res.status(500).json({ success: false, message: 'Failed to unhide', error: error.message });
+}
+  },
+
 };
 
 module.exports = hideController;
