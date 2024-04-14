f420c30a9e8a8531f5fa54dea33bd145c634a2ce
Zyad Sowilam <96208685+Zyadsowilam@users.noreply.github.com>
2024-04-14 22:02:14 +0200

    save comment/post

--- controllers/saveController.js
+++ controllers/saveController.js
@@ -1,23 +1,55 @@
+saveService=require('../services/saveService');
+
 const saveController = {
   savePostOrComment: async (req, res) => {
     try {
-      // const { type, id } = req.body;
-      // let itemType;
-      // switch (type) {
-      //     case 'post':
-      //         itemType = 'post';
-      //         break;
-      //     case 'comment':
-      //         itemType = 'comment';
-      //         break;
-      //     default:
-      //         return res.status(400).json({ success: false, message: 'Invalid item type' });
-      // }
+      const {entityId,type} = req.body;
+ 
+      let username =req.user;
+      
+      if (req.user?.iat){
+        username=req.user.username;
+      }
+  else{
+    username=req.user;
+  }
 
-      res.json({ success: true, message: 'Saved commern 6 successfully' });
-    } catch (error) {
-      res.status(500).json({ success: false, message: 'Failed to save post or comment', error: error.message });
-    }
+    const result=await saveService.savePostOrComment(entityId, username,type );
+    
+    if (result.success) {
+    
+    res.status(200).json({ message: 'saved  successfully.' });
+  } else {
+    res.status(400).json({ errors: result.errors, message: result.error });
+}
+} catch (error) {
+  res.status(500).json({ success: false, message: 'Failed to save', error: error.message });
+}
+  }, 
+  unsavePostOrComment: async (req, res) => {
+    try {
+      const {entityId,type} = req.body;
+ 
+      let username =req.user;
+      
+      if (req.user?.iat){
+        username=req.user.username;
+      }
+  else{
+    username=req.user;
+  }
+
+    const result=await saveService.unsavePostOrComment(entityId, username,type );
+    
+    if (result.success) {
+    
+    res.status(200).json({ message: 'unsaved successfully.' });
+  } else {
+    res.status(400).json({ errors: result.errors, message: result.error });
+}
+} catch (error) {
+  res.status(500).json({ success: false, message: 'Failed to unsave', error: error.message });
+}
   },
 };
 
