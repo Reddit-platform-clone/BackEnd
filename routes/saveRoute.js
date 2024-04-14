4cec71372ce67a8c26bc94af4fbe9479cf941344
Zyad Sowilam <96208685+Zyadsowilam@users.noreply.github.com>
2024-04-14 22:02:14 +0200

    save comment/post

--- routes/saveRoute.js
+++ routes/saveRoute.js
@@ -2,7 +2,8 @@ const express = require('express');
 
 const router = express.Router();
 const saveController = require('../controllers/saveController');
-
-router.post('/api/save', saveController.savePostOrComment);
+const authenticateToken = require('../middleware/authMiddleware');
+router.post('/api/save',authenticateToken, saveController.savePostOrComment);
+router.post('/api/unsave',authenticateToken, saveController.unsavePostOrComment);
 
 module.exports = router;
