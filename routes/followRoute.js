f60514f3e515cd832e13101956da3b96007fc299
Zyad Sowilam <96208685+Zyadsowilam@users.noreply.github.com>
2024-04-14 21:22:37 +0200

    follow

--- routes/followRoute.js
+++ routes/followRoute.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const router = express.Router();
 const followController = require('../controllers/followController');
-
-router.post('/api/follow_post', followController.followPost);
-
+const authenticateToken = require('../middleware/authMiddleware');
+router.post('/api/follow_post',authenticateToken, followController.followPost);
+router.post('/api/unfollow_post',authenticateToken, followController.unfollowPost);
 module.exports = router;
