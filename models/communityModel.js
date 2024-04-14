daa51365b133b415beb7ce89540a1a105a659539
Yousef Wael <yousef.wael2002@gmail.com>
2024-04-14 18:08:49 +0200

    removed schemas folder

--- models/communityModel.js
+++ models/communityModel.js
@@ -1,15 +1,15 @@
 const mongoose = require('mongoose');
 
 const communitySchema = new mongoose.Schema({
-    title: { type: String, unique: true, required: true },
+    communityName: { type: String, unique: true, required: true },
     type: { type: String, enum: ['private', 'ristricted', 'public']},
     isNSFW: { type: Boolean, default: false },
-    description: String,
+    description: {type: String},
     rules: { type: [String], default: [] },
     banned: { type: [String], default: [] },
     moderatorsUsernames: { type:[String], required:true },
     members: { type: [String], default: [] }
-}, { strict: 'throw' });
+}, { strict: 'throw', timestamps: true });
 
 const Communities = mongoose.model('Communities', communitySchema);
 
