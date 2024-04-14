f9f3d18d27497eaa57c8b5d808ec00e551987cd4
Yousef Wael <yousef.wael2002@gmail.com>
2024-04-14 11:24:56 +0200

    created list community service

---
+++ services/listCommunityService.js
@@ -0,0 +1,13 @@
+const Community = require('../schemas/communitySchema.js');
+
+const listCommunityService = {
+    listCommunities: async () => {
+        try {
+            const communities = await Community.find();
+            return communities;
+        } catch (error) {
+            console.error('Error fetching communities', error);
+            throw new Error('Failed to fetch communities');
+        }
+    }
+}
