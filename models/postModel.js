9da0629e20eee7371a904ec1ad6869c145749559
Zyad Sowilam <96208685+Zyadsowilam@users.noreply.github.com>
2024-04-15 21:54:07 +0200

    Update postModel.js

--- models/postModel.js
+++ models/postModel.js
@@ -2,21 +2,23 @@ let mongoose = require('mongoose');
 
 const postSchema = new mongoose.Schema({
     postId: {type: String, unique: true},
-    content: { type: String},
-    title: {type: String},
+    content: { type: String },
+    title: {type: String, required: true},
     userId: {type: String, required: true},
-    parentId: {type: String, required: true, default:"0"},
+    parentId: {type: Number, required: true},
     media: {type: JSON},
-    downvotes: {type: Number, default:0},
-    communityId: {type: String, required: true},
+    downvotes: {type: Number,default:0},
+    communityId: {type: Number, required: true},
     communityName: {type: String},
-    upvotes: {type: Number, default:0},
-    numComments: {type: Number, default:0},
-    scheduled: {type: Boolean, default:0},
-    isSpoiler: {type: Boolean, default:0},
-    numViews: {type: Number, default:0},
+    communityImage:{type:JSON},
+    upvotes: {type: Number,default:0},
+    numComments: {type: Number,default:0},
+    scheduled: {type: Boolean},
+    isSpoiler: {type: Boolean},
+    numViews: {type: Number, required: true},
     isLocked: {type: Boolean, required: true},
-    
+
+
 }, {timestamps: true});
 
 const Post = mongoose.model('post', postSchema);
