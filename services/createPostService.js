34de5f17fd872b8fadc2d80136100d157a1b4080
RadwaAbdelfattah <101428006+RadwaAbdelfattah@users.noreply.github.com>
2024-04-15 02:02:45 +0200

    createPost edit

--- services/createPostService.js
+++ services/createPostService.js
@@ -3,14 +3,13 @@ const User = require('../models/userModel');
 const { v4: uuidv4 } = require('uuid'); // Import the uuid library
 
 const createPostService = {
-    createPost: async (postData, username) => {
+    createPost: async (postData) => {
         try {
             // Generate a unique post ID using UUID
             const postId = uuidv4();
 
             // Add the generated post ID and user ID to the post data
             postData.postId = postId;
-            postData.userId = username;
 
             // Create a new post object using the updated postData
             const newPost = new Post(postData);
@@ -23,24 +22,6 @@ const createPostService = {
         } catch (error) {
             console.error("Error creating post:", error);
             throw new Error("Failed to create post");
-        }
-    },
-    getPostById: async (postId) => {
-        try {
-            // Find the post by its ID
-            const post = await Post.findOne({ postId });
-
-            // If the post is not found, return null
-            if (!post) {
-                
-                throw new Error(`Post with ID ${postId} not found`);
-            }
-
-            // Return the found post
-            return post;
-        } catch (error) {
-            console.error(`Error fetching post with ID ${postId}:`, error);
-            throw new Error("Failed to fetch post");
         }
     }
 };
