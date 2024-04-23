d98c8a94ddaae24d68e5c88a021b1eeed2a76584
Zyad Sowilam <96208685+Zyadsowilam@users.noreply.github.com>
2024-04-23 19:35:59 +0200

    Create liveMessagesController.js

---
+++ controllers/liveMessagesController.js
@@ -0,0 +1,62 @@
+//read all messages
+//del message
+//send message
+
+const { request } = require('express');
+const messageService = require('../services/liveMessagesService');
+const jwt = require('jsonwebtoken');
+const liveMessagesController = {
+    compose: async (req, res) => {
+      try {
+        let username =req.user;
+        // console.log(req.user.iat);
+        if (req.user?.iat){
+          username=req.user.username;
+        }
+    else{
+      username=req.user;
+    }
+  
+        const { recipient, from, title, content } = req.body;
+  
+        const result = await messageService.composeMessage({ username, recipient, from, title, content });
+  
+        if (result.success) {
+            res.status(200).json({ message: result.message });
+        } else {
+            res.status(400).json({ errors: result.errors, message: result.error });
+        }
+    } catch (error) {
+        console.error('Error composing message:', error);
+        res.status(500).json({ error: 'Failed to send message.' });
+    }
+    },
+    
+  
+    getInboxMessages: async (req, res) => {
+      try {
+        let username =req.user;
+        // console.log(req.user.iat);
+        if (req.user?.iat){
+          username=req.user.username;
+        }
+    else{
+      username=req.user;
+    }
+    
+        const inboxMessages = await messageService.getInboxMessages(username);
+       
+        if (inboxMessages.success) {
+         
+        res.status(200).json(inboxMessages.message);
+      } else {
+        res.status(400).json({ errors: inboxMessages.errors, message: inboxMessages.error });
+    }
+      } catch (error) {
+        
+        console.error('Failed to retrieve inbox messages:', error);
+        res.status(500).json({ error: 'Failed to retrieve inbox messages.' });
+      }
+    },};
+
+    module.exports = liveMessagesController;
