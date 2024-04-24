6d7087a9f572da26a712c03618390ba9dd9b163e
Zyad Sowilam <96208685+Zyadsowilam@users.noreply.github.com>
2024-04-24 22:52:01 +0200

    Update messageRoute.js

--- routes/messageRoute.js
+++ routes/messageRoute.js
@@ -14,14 +14,7 @@ router.get('/api/message/sent',userAuthentication.authorizeationToken, messageCo
 router.post('/api/unread_message',userAuthentication.authorizeationToken, messageController.markMessageUnread);
 router.post('/api/read_all_messages',userAuthentication.authorizeationToken, messageController.markAllMessagesRead);
 router.get('/api/get_user_mentions', userAuthentication.authorizeationToken,messageController.getUserMentions);
-router.post('/api/message/compose',authenticateToken, messageController.compose);
-router.get('/api/message/inbox',authenticateToken, messageController.getInboxMessages);
-router.get('/api/message/unread',authenticateToken, messageController.getUnreadMessages);
-router.delete('/api/message/del_msg',authenticateToken, messageController.deleteMessage);
-router.post('/api/report_msg',authenticateToken, messageController.reportMessage);
-router.get('/api/message/sent',authenticateToken, messageController.getSentMessages);
-router.post('/api/unread_message',authenticateToken, messageController.markMessageUnread);
+
 router.post('/api/read_message',authenticateToken, messageController.markMessageRead);
-router.post('/api/read_all_messages',authenticateToken, messageController.markAllMessagesRead);
-router.get('/api/get_user_mentions', messageController.getUserMentions);
+
 module.exports = router;
