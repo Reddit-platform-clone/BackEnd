0bb8912c92937cd4bc7c232a66c4d217b4b1d9c1
Zyad Sowilam <96208685+Zyadsowilam@users.noreply.github.com>
2024-04-25 17:09:51 +0200

    Update postModel.js

--- models/postModel.js
+++ models/postModel.js
@@ -1,22 +1,25 @@
 let mongoose = require('mongoose');
 
 const postSchema = new mongoose.Schema({
-    postId: {type: String, unique: true},
+
     content: { type: String },
     title: {type: String, required: true},
     userId: {type: String, required: true},
-    parentId: {type: String, required: true},
     media: {type: JSON},
     downvotes: {type: Number,default:0},
     communityId: {type: String, required: true},
-    communityName: {type: String},
-    communityImage:{type:JSON},
     upvotes: {type: Number,default:0},
-    numComments: {type: Number,default:0},
     scheduled: {type: Boolean},
-    isSpoiler: {type: Boolean},
-    numViews: {type: Number, required: true},
-    isLocked: {type: Boolean, required: true},
+    isSpoiler: {type: Boolean,default:false},
+    isLocked: {type: Boolean,default:false},
+    isReported: {type: Boolean,default:false},
+    isReason:{type:String,default:false},
+    nsfw:{type: Boolean,default:false},
+    ac:{type: Boolean,default:false},
+    url:{  type: [String],
+        default: []  },
+    flair:{  type: [String],
+        default: []  }, 
 
 
 }, {timestamps: true});
