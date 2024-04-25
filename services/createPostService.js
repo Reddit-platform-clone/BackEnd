a86ab3b2fc78ea5e0f26b584df806c84a2b32f3f
Zyad Sowilam <96208685+Zyadsowilam@users.noreply.github.com>
2024-04-25 18:09:49 +0200

    mention in post

--- services/createPostService.js
+++ services/createPostService.js
@@ -2,11 +2,11 @@ const Post = require('../models/postModel');
 const User = require('../models/userModel');
 const Community = require('../models/communityModel.js')
 const { v4: uuidv4 } = require('uuid'); // Import the uuid library
-
+const Mention=require('../models/mentionModel');
 const createPostService = {
     createPost: async (postData,username) => {
         try {
-            console.log(postData)
+            
             if(!postData.title || !postData.communityId){
                 return { success: false, error: `title or communityId is null.` };
             }
@@ -23,9 +23,37 @@ const createPostService = {
 
         community.posts.push(savedPost._id)
         await community.save()
+        if(postData.content){
+            const mentionRegex = /@(\w+)/g;
+        
+            const mentions =postData.content.match(mentionRegex);
+            const savedmention=[]
+            if (mentions) {
+                let mentiondata=mentions.map(mention => mention.substring(1));
+                
+                for (const checkUser of mentiondata){
+                    const userChecker= await User.findOne({username:checkUser})
+                    if (userChecker){
+                        const mention = new Mention({ mentionedBy:username,mentioned:checkUser,type:"post",entityId:savedPost._id });
+                        
+                        mention.save();
+                        savedmention.push(checkUser);
+                        
+                    }
+                }
+                if (savedmention.length === mentiondata.length) {
+                    return { success: true, message: 'post sent successfully with mentions.' };
+                }
+                else{
+                    return { success: true, message: 'post sent successfully but some mentioned users do not exist',missingUsers: mentiondata.filter(username => !savedmention.includes(username)) };
+                }
+                
+            }
+    
 
+        }
             
-            return { success: true, message: `Post created succesfully` };
+        return { success: true, message: `Post created succesfully` };
         } catch (error) {
             console.error("Error creating post:", error);
             throw new Error("Failed to create post");
