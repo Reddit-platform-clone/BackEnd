610e2cca7babd6edd75081baadf37b57e5aa4230
Zyad Sowilam <96208685+Zyadsowilam@users.noreply.github.com>
2024-05-04 23:26:07 +0300

    aPost

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,5 +1,5 @@
 const postService = require('../services/postService');
-
+const enrichPostsWithExtras  = require('../services/modifierPostService.js');
 const postController = {
 
   getPostReplies: async (req, res) => {
@@ -12,6 +12,21 @@ const postController = {
         res.status(200).json({ message: result.message });
     } else {
         res.status(400).json({ errors: result.errors, message: result.error });
+    }
+    } catch (error) {
+      res.status(500).json({ success: false, message: 'Failed to retrieve post replies', error: error.message });
+    }
+  },
+  getAPost: async (req, res) => {
+    try {
+  
+      let postID=req.body.postID;
+     
+      const post=await enrichPostsWithExtras([postID]);
+      if (post) {
+        res.status(200).json({post });
+    } else {
+        res.status(400).json({ success: false, message: "post doesnt exist"});
     }
     } catch (error) {
       res.status(500).json({ success: false, message: 'Failed to retrieve post replies', error: error.message });
--- routes/postRoute.js
+++ routes/postRoute.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const postController = require('../controllers/postController');
 
 router.post('/api/get_post_replies', postController.getPostReplies);
+router.post('/api/getAPost', postController.getAPost);
 
 module.exports = router;
