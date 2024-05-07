81f500fbb8caedc8fd8f4cf9fd4e27f1e6abb338
Yousef Wael <yousef.wael2002@gmail.com>
2024-05-07 19:24:44 +0300

    feat: pagination in all listings

--- controllers/subredditController.js
+++ controllers/subredditController.js
@@ -4,35 +4,41 @@ const subredditService = require('../services/subredditService.js');
 const subredditController = {
     getAllPosts: async (req, res) => {
         try {
-            const posts = await subredditService.getAll();
-            res.json({success: true, data: posts});
+            const page = req.query.page || 1;
+            const limit = req.query.limit || 20;
+            const posts = await subredditService.getAll(page, limit);
+            res.json({ success: true, data: posts });
         } catch (error) {
-            res.status(500).json({success: false, error: error.message});
+            res.status(500).json({ success: false, error: error.message });
         }
     },
 
     getBestPost: async (req, res) => {
         try {
-            const bestPost = await subredditService.getBest();
-            res.json({ succes: true, data: bestPost });
-        } catch (err) {
-            res.status(500).json({  success: false, error: err.message })
+            const page = req.query.page || 1;
+            const limit = req.query.limit || 20;
+            const bestPost = await subredditService.getBest(page, limit);
+            res.json({ success: true, data: bestPost });
+        } catch (error) {
+            res.status(500).json({ success: false, error: error.message });
         }
     },
 
     getHotPost: async (req, res) => {
         try {
-            const hotPost = await subredditService.getHot();
-            res.json({ succes: true, data: hotPost });
-        } catch (err) {
-            res.status(500).json({  success: false, error: err.message })
+            const page = req.query.page || 1;
+            const limit = req.query.limit || 20;
+            const hotPost = await subredditService.getHot(page, limit);
+            res.json({ success: true, data: hotPost });
+        } catch (error) {
+            res.status(500).json({ success: false, error: error.message });
         }
     },
 
     getNewPost: async (req, res) => {
         try {
             const page = req.query.page || 1;
-            const limit = req.query.limit || 10;
+            const limit = req.query.limit || 20;
             const newPost = await subredditService.getNew(page, limit);
             res.json({ success: true, data: newPost });
         } catch (err) {
@@ -42,19 +48,23 @@ const subredditController = {
 
     getTopPost: async (req, res) => {
         try {
-            const topPost = await subredditService.getTop();
-            res.json({ succes: true, data: topPost });
-        } catch (err) {
-            res.status(500).json({  success: false, error: err.message })
+            const page = req.query.page || 1;
+            const limit = req.query.limit || 20;
+            const topPost = await subredditService.getTop(page, limit);
+            res.json({ success: true, data: topPost });
+        } catch (error) {
+            res.status(500).json({ success: false, error: error.message });
         }
     },
 
     getRandomPost: async (req, res) => {
         try {
-            const randomPost = await subredditService.getRandom();
-            res.json({ succes: true, data: randomPost });
-        } catch (err) {
-            res.status(500).json({  success: false, error: err.message })
+            const page = req.query.page || 1;
+            const limit = req.query.limit || 20;
+            const randomPost = await subredditService.getRandom(page, limit);
+            res.json({ success: true, data: randomPost });
+        } catch (error) {
+            res.status(500).json({ success: false, error: error.message });
         }
     },
 };
